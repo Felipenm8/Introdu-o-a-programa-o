bcf64b87071bab56ddedb04d7218de9cb608a9db
Felipenm8 <161980733+Felipenm8@users.noreply.github.com>
2024-04-19 08:15:29 -0300

    questão 5
    .

---
+++ questão 5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	int ano;
+}   setlocale(LC ALL, "portuguse")
+cout<<"informe um ano";
+cin>>ano;
+resto = ano % 4;
+if( resto==0) {
+	cout>> "ano bissexto";
+}else{
+	cout<<"não é bissexto"
+}
+return 0
