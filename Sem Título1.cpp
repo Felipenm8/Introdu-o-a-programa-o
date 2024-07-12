e989ee2f66524fcf7845329ad0891857d30a34db
Felipenm8 <161980733+Felipenm8@users.noreply.github.com>
2024-07-12 09:01:21 -0300

    .
    .

---
+++ Sem Título1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+ int main( ) {
+ 	
+ 	float a , b, media;
+ 	
+ 	cin>>b;
+ 	cout<<"informe o numero";
+ 	cin>>a;
+ 	cout<<"informe o segundo numero";
+ 	cin>>b;
+ 	media= (a+b) /2;
+ 	
+ 	cout<<"media"<<endl;
+ 	
+ 	
+	return 0;
+	
+}
---
+++ questao1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+ int main( ) {
+ 	
+ 	float raio,c;
+ 	cin>>raio;
+ 	c = 2 * 3.14 * raio;
+ 	
+ 	cout<<"circuferencia:"<<endl;
+ 	
+ 	
+	return 0;
+	
+}
